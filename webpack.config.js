1ec94d2590622fdd19f52447b164b3cf081b6a69
myungmin <audals2478@gmail.com>
2020-08-13 16:43:21 +0900

    css 파일 분리

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   // entry files
